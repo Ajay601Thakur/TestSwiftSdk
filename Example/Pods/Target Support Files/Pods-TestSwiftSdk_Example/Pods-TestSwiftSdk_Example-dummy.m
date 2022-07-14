5d1f3d294b29ebed73df9a69e2cdfc71e9ed485d
ajay <ajay01493@gmail.com>
2022-07-14 12:28:12 +0530

    fixed Some Minor Task

---
+++ Example/Pods/Target Support Files/Pods-TestSwiftSdk_Example/Pods-TestSwiftSdk_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestSwiftSdk_Example : NSObject
+@end
+@implementation PodsDummy_Pods_TestSwiftSdk_Example
+@end
---
+++ Example/Pods/Target Support Files/TestSwiftSdk/TestSwiftSdk-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TestSwiftSdk : NSObject
+@end
+@implementation PodsDummy_TestSwiftSdk
+@end
